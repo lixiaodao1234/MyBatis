95277731be77341be4e39c26c178cbdf152c6d76
lixiaodao1234 <82818628+lixiaodao1234@users.noreply.github.com>
2022-04-15 11:38:04 +0800

    pull commit

--- MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
+++ MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
@@ -14,6 +14,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello master test");
         System.out.println("hot fix test");
-
+        System.out.println("hello github");
     }
 }
