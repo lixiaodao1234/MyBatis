348fe1d553782b84fc65750ccce0a9f8f61be38c
lixiaodao <1755594515@qq.com>
2022-04-15 10:20:07 +0800

    third commit

--- MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
+++ MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
@@ -12,6 +12,10 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git3");
+        System.out.println("hello git3");
+        System.out.println("hello git3");
+
 
     }
 }
