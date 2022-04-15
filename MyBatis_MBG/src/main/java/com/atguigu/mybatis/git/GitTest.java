81482c58ddc0b1ed6f275dcb782b4c4807664b74
lixiaodao <1755594515@qq.com>
2022-04-15 10:26:36 +0800

    hot-fix commit1

--- MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
+++ MyBatis_MBG/src/main/java/com/atguigu/mybatis/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hot fix");
 
     }
 }
